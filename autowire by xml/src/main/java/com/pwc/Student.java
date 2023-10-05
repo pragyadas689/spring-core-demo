270749b5698337ad77fce76fc565f0f1f887b2f8
Pragya Das <pragyadas689@gmail.com>
2023-10-05 11:15:47 +0530

    Add files via upload

---
+++ autowire by xml/src/main/java/com/pwc/Student.java
@@ -0,0 +1,20 @@
+package com.pwc;
+
+public class Student {
+    private Course course;
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "course=" + course +
+                '}';
+    }
+}
